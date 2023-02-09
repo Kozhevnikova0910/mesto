ac374ad70c0030534c924f04421e071f0e4ed2fe
Kozhevnikova0910 <kozhevnikova0910@gmail.com>
2023-02-09 14:15:00 +0300

    fix/исправить замечания ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,18 @@
 const popupEditProfile = document.querySelector('.popup_type_edit');
 const openEditProfileButton = document.querySelector('.profile__edit-btn');
 const closeEditProfileButton = popupEditProfile.querySelector('.popup__close-btn');
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
-let inputEditProfileName = popupEditProfile.querySelector('.popup__input_type_name');
-let inputEditProfileDescription = popupEditProfile.querySelector('.popup__input_type_about');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+const inputEditProfileName = popupEditProfile.querySelector('.popup__input_type_name');
+const inputEditProfileDescription = popupEditProfile.querySelector('.popup__input_type_about');
 
 // Переменные попапа Add Place
 const popupAddPlace = document.querySelector('.popup_type_add');
+const formAddPlace = popupAddPlace.querySelector('.popup__form')
 const openAddPlaceButton = document.querySelector('.profile__add-btn');
 const closeAddPlaceButton = popupAddPlace.querySelector('.popup__close-btn');
-let inputAddPlaceName = popupAddPlace.querySelector('.popup__input_type_name');
-let inputAddPlaceLink = popupAddPlace.querySelector('.popup__input_type_link');
+const inputAddPlaceName = popupAddPlace.querySelector('.popup__input_type_name');
+const inputAddPlaceLink = popupAddPlace.querySelector('.popup__input_type_link');
 
 // Переменные попапа Picture
 const popupPicture = document.querySelector('.popup_type_picture');
@@ -28,11 +29,11 @@ const placesContainer = document.querySelector('.places');
 // Функции открытия попапа
 function openPopup (element) {
     element.classList.add('popup_opened');
-    inputEditProfileName.value = profileName.textContent;
-    inputEditProfileDescription.value = profileDescription.textContent;
 }
 
 function openPopupEditProfile () {
+    inputEditProfileName.value = profileName.textContent;
+    inputEditProfileDescription.value = profileDescription.textContent;
     openPopup(popupEditProfile);
 }
 
@@ -40,7 +41,9 @@ function openPopupAddPlace () {
     openPopup(popupAddPlace);
 }
 
-function openPopupPicture () {
+function openPopupPicture (name, image) {
+    imagePopupPicture.src = image.src;
+    titlePicture.textContent = name.textContent;
     openPopup(popupPicture);
 }
 
@@ -69,14 +72,38 @@ function saveProfileChanges (e) {
     closePopup(popupEditProfile);
 }
 
-function addPlace(name, link) {
+function addLikeHandler(item) {
+    item.addEventListener('click', e => e.target.classList.toggle('place__like_active'))
+}
+
+function addRemovePlaceHandler(item) {
+    item.addEventListener('click', e => e.target.parentElement.remove())
+}
+
+function addPlace(placeData) {
     const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
-    placeElement.querySelector('.place__name').textContent = name;
-    placeElement.querySelector('.place__image').src = link;
-    addLikeHandler(placeElement.querySelector('.place__like'));
-    addRemovePlaceHandler(placeElement.querySelector('.place__delete'));
-    placeElement.querySelector('.place__image').addEventListener('click', openImageClick);
+    const placeNameElement = placeElement.querySelector('.place__name');
+    const placeImageElement = placeElement.querySelector('.place__image');
+    const placeLikeElement = placeElement.querySelector('.place__like');
+    const placeDeleteElement = placeElement.querySelector('.place__delete');
+    placeNameElement.textContent = placeData.name;
+    placeImageElement.src = placeData.link;
+    addLikeHandler(placeLikeElement);
+    addRemovePlaceHandler(placeDeleteElement);
+    placeImageElement.addEventListener('click',() => openPopupPicture(placeNameElement, placeImageElement));
+    return placeElement;
+}
+
+function renderCard(place) {
+    const placeElement = addPlace(place);
     placesContainer.prepend(placeElement);
+}
+
+function popupAddPlaceSubmit(e) {
+    e.preventDefault();
+    renderCard({name: inputAddPlaceName.value, link: inputAddPlaceLink.value});
+    closePopupAddPlace();
+    formAddPlace.reset();
 }
 
 
@@ -88,71 +115,12 @@ popupEditProfile.addEventListener('submit', saveProfileChanges);
 // События попапа Add Place
 openAddPlaceButton.addEventListener('click', openPopupAddPlace);
 closeAddPlaceButton.addEventListener('click', closePopupAddPlace);
-popupAddPlace.addEventListener('submit', function (e) {
-    e.preventDefault();
-    if (inputAddPlaceName.value && inputAddPlaceLink.value) {
-        addPlace(inputAddPlaceName.value, inputAddPlaceLink.value);
-        closePopupAddPlace();
-        inputAddPlaceName.value = '';
-        inputAddPlaceLink.value = '';
-    }
-});
+popupAddPlace.addEventListener('submit', popupAddPlaceSubmit);
 
-// Вешаем клик на лайки из HTML, на остальные лайки клик вешаем при добавлении
-const likesArray = document.querySelectorAll('.place__like')
-likesArray.forEach(item => addLikeHandler(item));
-
-function addLikeHandler(item) {
-    item.addEventListener('click', e => e.target.classList.toggle('place__like_active'))
-}
-
-
-// Добавление карточек мест
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
-
-initialCards.forEach(place => addPlace(place.name, place.link));
-
-// Вешаем клик на иконку удаления места
-const removeButtonsArray = document.querySelectorAll('.place__delete')
-removeButtonsArray.forEach(item => addRemovePlaceHandler(item));
-
-function addRemovePlaceHandler(item) {
-    item.addEventListener('click', e => e.target.parentElement.remove())
-}
-
-// Вешаем клик на фуллскрин места
-const imagesArray = document.querySelectorAll('.place__image');
-imagesArray.forEach(item => item.addEventListener('click', openImageClick));
-
-function openImageClick(e) {
-    openPopupPicture();
-    imagePopupPicture.src = e.target.src;
-    titlePicture.textContent = e.target.nextElementSibling.firstElementChild.textContent;
-}
-
+// События попапа Picture
 closePictureButton.addEventListener('click', closePopupPicture);
+
+// Добавляем карточки из массива
+initialCards.forEach(function (place) {
+    renderCard(place);
+});
